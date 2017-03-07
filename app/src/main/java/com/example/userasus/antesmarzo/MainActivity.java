b59c14512f5ae8cf85951221b2b24e0a4551fe9b
RoyerUnivalle <royer.estrada@correounivalle.edu.co>
2017-03-07 09:11:02 -0500

    segundo commit

--- app/src/main/java/com/example/userasus/antesmarzo/MainActivity.java
+++ app/src/main/java/com/example/userasus/antesmarzo/MainActivity.java
@@ -11,26 +11,52 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity  implements  View.OnClickListener{
-
-
     EditText input;
     TextView campo;
     Button oc1;
     Button oc2;
-
+    Button btn_iniciar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        oc1 = (Button) findViewById(R.id.oc1);
+        oc2 = (Button) findViewById(R.id.oc2);
 
+        btn_iniciar= (Button) findViewById(R.id.btn_iniciar);
+        //metodo delegado
+        oc1.setOnClickListener(this);
 
+        btn_iniciar.setOnClickListener(this);
+
+        //metodo en linea
+        oc2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,"hola meotod inline",Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()){
+            case R.id.oc1:
+                Toast.makeText(this,"hola meotod delegado",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btn_iniciar:
+                Iniciar();
+                break;
+            default:
+                break;
+        }
     }
 
+    public void Iniciar(){
+        Intent ir = new Intent(MainActivity.this,Main2Activity.class);
+        ir.addFlags(ir.FLAG_ACTIVITY_CLEAR_TOP | ir.FLAG_ACTIVITY_SINGLE_TOP);
+        startActivity(ir);
+    }
     public void Saludar(View v){
         //enlace
         input = (EditText) findViewById(R.id.ed1);
